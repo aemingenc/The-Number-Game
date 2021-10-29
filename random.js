9292084905e2431e0ec2bc566728a1e7a6e54cb9
aemingenc <aemingenc@gmail.com>
2021-10-29 22:16:26 +0300

    First Commit

---
+++ random.js
@@ -0,0 +1,28 @@
+// alert( "Please push start button for the game and write your gues ");
+let button =document.getElementById("check");
+let content =document.getElementById("content");
+rNum=Math.floor(Math.random() * 100);
+button.addEventListener("click",() => {
+    let guess=document.getElementById("search_bar").value;
+    
+    
+        console.log(rNum);
+    content.innerHTML =""
+        if(guess == rNum){
+            content.innerHTML += `<p>WİNNER</p>`;
+            rNum=Math.floor(Math.random() * 100);
+    }
+        else if(guess< rNum){
+            content.innerHTML += `<p>UP</p>`;
+    }
+        else if(guess>rNum){
+            content.innerHTML += `<p>DOWN</p>`;
+    }
+        else{
+            content.innerHTML += "<p>WRONG KEYS.TRY AGAIN</p>";
+    }
+
+   
+})
+  
+
