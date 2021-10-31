26ea6e26ad7ecbc83b8a6e54d6d14637ecfd5063
aemingenc <aemingenc@gmail.com>
2021-10-31 23:50:57 +0200

    explain

--- random.js
+++ random.js
@@ -7,21 +7,25 @@ button.addEventListener("click",() => {
 
         console.log(rNum);
     content.innerHTML =""
-    
-    
+    const count=0;
         if(guess == rNum){
             content.innerHTML += `<p>WİNNER</p>`;
             rNum=Math.floor(Math.random() * 100);
+            
     }
         else if(guess< rNum){
             content.innerHTML += `<p>UP</p>`;
+        
     }
         else if(guess>rNum){
             content.innerHTML += `<p>DOWN</p>`;
+            
     }
         else{
             content.innerHTML += "<p>WRONG KEYS.TRY AGAIN</p>";
+            
     }
+
 
 
    
