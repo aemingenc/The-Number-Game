75356847fa199b308c26c40b2757653d8125c12b
aemingenc <aemingenc@gmail.com>
2021-11-01 00:54:41 +0200

    enter-value

--- random.js
+++ random.js
@@ -1,13 +1,17 @@
 // alert( "Please push start button for the game and write your gues ");
 let button =document.getElementById("check");
 let content =document.getElementById("content");
+let guess=document.getElementById("search_bar").value;
 rNum=Math.floor(Math.random() * 100);
 button.addEventListener("click",() => {
     let guess=document.getElementById("search_bar").value;
-
+    if(guess==""){
+        alert("please enter to do");
+    }
+    
+    else{
         console.log(rNum);
-    content.innerHTML =""
-    const count=0;
+        content.innerHTML ="";
         if(guess == rNum){
             content.innerHTML += `<p>WİNNER</p>`;
             rNum=Math.floor(Math.random() * 100);
@@ -24,11 +28,36 @@ button.addEventListener("click",() => {
         else{
             content.innerHTML += "<p>WRONG KEYS.TRY AGAIN</p>";
             
+    }}});
+guess2.addEventListener("keyup",(e)=>{
+    let guess=document.getElementById("search_bar").value;
+    content.innerHTML ="";
+        if(e.code=="Enter"){
+            e.preventDefault();
+            if(guess==""){
+                alert("please enter to do");
+            }
+            else{
+                if(guess == rNum){
+                    content.innerHTML += `<p>WİNNER</p>`;
+                    rNum=Math.floor(Math.random() * 100);
+            
     }
-
+                else if(guess< rNum){
+                    content.innerHTML += `<p>UP</p>`;
+        
+    }
+                else if(guess>rNum){
+                    content.innerHTML += `<p>DOWN</p>`;
+            
+    }
+                else{
+                    content.innerHTML += "<p>WRONG KEYS.TRY AGAIN</p>";
+            
+    }}}
 
 
    
-})
+});
   
 
